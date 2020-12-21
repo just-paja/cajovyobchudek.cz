e9187b53a07e5f828920a8158fa4f9b089552f4b
Pavel Žák <pavel@zak.global>
2020-12-21 13:31:29 +0100

    Drop duplicate settings path

--- cajovyobchudek/settings.py
+++ cajovyobchudek/settings.py
@@ -172,9 +172,6 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS = (
-    BASE_DIR / 'website' / 'static',
-)
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
