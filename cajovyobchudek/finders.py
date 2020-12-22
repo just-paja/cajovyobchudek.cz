70c837f385b952ba850d4057c609e3a5256a5ef2
Pavel Žák <pavel@zak.global>
2020-12-22 10:28:41 +0100

    Fix linting issues

--- cajovyobchudek/finders.py
+++ cajovyobchudek/finders.py
@@ -3,7 +3,7 @@ from django.contrib.staticfiles.finders import FileSystemFinder
 
 
 class CssFinder(FileSystemFinder):
-    def find(self, path, all=False): # pylint:disable=redefined-builtin
+    def find(self, path, all=False):  # pylint:disable=redefined-builtin
         # Assume the file already exists given we are running on AWS
         if settings.AWS_ACCESS_KEY_ID and (path.endswith('.css') or path.endswith('.css.map')):
             return path
