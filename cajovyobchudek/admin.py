079fe4f025310a7f9f3bd1a9e49ab32238aad9c7
just-paja <pavel@zak.global>
2020-12-23 14:14:12 +0100

    Fix linting issues

--- cajovyobchudek/admin.py
+++ cajovyobchudek/admin.py
@@ -7,6 +7,7 @@ from . import models
 class BusinessHoursAdmin(ModelAdmin):
     list_display = ('weekday', 'from_hour', 'to_hour')
 
+
 @register(models.ClosingRules)
 class ClosingRulesAdmin(ModelAdmin):
     list_display = ('start', 'end', 'reason')
