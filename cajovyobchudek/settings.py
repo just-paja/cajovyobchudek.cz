1fa9539eaaa643227d5b383a40e94eede47f53da
just-paja <pavel@zak.global>
2020-12-27 17:17:54 +0100

    Use statc files storage

--- cajovyobchudek/settings.py
+++ cajovyobchudek/settings.py
@@ -177,7 +177,7 @@ LOCALE_PATHS = [
 
 if AWS_ACCESS_KEY_ID:
     DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
     DBBACKUP_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
     COMPRESS_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     DBBACKUP_STORAGE_OPTIONS = {
